d1059087e13e7d678fd914ac6b7aacf08c307a7c
Divyang2599 <loda0002@algonquinlive.com>
2026-06-18 19:40:55 -0400

    Add Terratest tests for NIC existence/attachment and Ubuntu version

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,27 +10,35 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "f13ebc34-ccd6-410f-92e4-27d4c7ffb834"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../",
-		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "loda0002",
 		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// --- Test 1: Confirm the NIC exists and is attached to the VM ---
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
+	assert.True(t, azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID))
+
+	vmNics := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
+	assert.Contains(t, vmNics, nicName)
+
+	// --- Test 2: Confirm the VM is running the correct Ubuntu version ---
+	vmImage := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
+	assert.Equal(t, "0001-com-ubuntu-server-jammy", vmImage.Offer)
+	assert.Equal(t, "22_04-lts-gen2", vmImage.SKU)
 }
